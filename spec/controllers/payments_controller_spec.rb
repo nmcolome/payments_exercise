9cb454654a098780f5e5e79db5df51f1e2d0e825
NataliaColome <nmcolome@gmail.com>
2017-09-25 15:06:09 -0600

    Build initial PaymentsController spec

---
+++ spec/controllers/payments_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PaymentsController, type: :controller do
+  describe '#create' do
+    let(:loan) { Loan.create!(funded_amount: 100.0) }
+    let(:valid_params) { { payment_amount: 20.0, payment_date: Date.today } }
+
+    context 'payment_amount < outstanding_balance' do
+      it 'responds with a 200' do
+        post :create, { loan_id: loan.id, payment: valid_params, format: :json }
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
